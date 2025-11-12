7f913dcaa4147480707920a9fcfa993d340c87d0
ayush02303 <badolaayush295@gmail.com>
2025-11-12 21:12:03 +0530

    changes accordian

--- miscell_projects/src/components/accordian/index.jsx
+++ miscell_projects/src/components/accordian/index.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 import data from "./data";
+import "./index.css"
 
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
+  const [ enableMultiSelection , setEnableMultiSelection ] = useState( false )
+  const [ multiple , setMultiple] = useState([])
 
   const handlesingleSelection = ( currId)=>{
     console.log(currId)
+    setSelected(currId)
+
+  }
+  const handleMultipleSelection = ( currId )=>{
 
   }
   return (
     <div className="wrapper">
+      <button onClick={()=> setEnableMultiSelection(!enableMultiSelection)}>Enable multi selection </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataitem) => (
@@ -18,6 +26,12 @@ const Accordian = () => {
               <h3>{dataitem.question}</h3>
               <span>+</span>
               </div>
+              {
+                selected === dataitem.id ? 
+                <div className="content"> {dataitem.answer}</div>
+                : 
+                null
+              }
             </div>
           ))
         ) : (
